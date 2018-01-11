9b66e17f312b94e49058b3323b620a53c52807af
Vlastik <vlastikczech1@gmail.com>
2018-01-10 20:23:32 -0500

    Update layout of the site

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,4 @@
 class Quote < ApplicationRecord
-  validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
-  validates :author, presence: true, length: { maximum: 50, minimum: 3 }
+  validates :saying,  length: { maximum: 140, minimum: 3 }
+  validates :author,  length: { maximum: 50, minimum: 3 }
 end
