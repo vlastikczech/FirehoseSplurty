26c2d41942ce5861257d61d620fc29f38fd04844
vlastikczech1 <vlastikczech1@gmail.com>
2018-01-16 18:44:02 -0500

    updated about image

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,8 @@ ActiveRecord::Schema.define(version: 20180109023104) do
   enable_extension "plpgsql"
 
   create_table "quotes", force: :cascade do |t|
-    t.string   "saying"
-    t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "saying"
+    t.string "author"
   end
 
 end
